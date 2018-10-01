0e887e255ab0681dfe6ecf60314ca06add6a716c
WikiDru <wikidru@gmail.com>
2018-10-02 01:19:09 +0900

    [#3] Add api : /app/env

--- src/main/java/org/code13k/helios/lib/Util.java
+++ src/main/java/org/code13k/helios/lib/Util.java
@@ -1,12 +1,5 @@
 package org.code13k.helios.lib;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.jar.Attributes;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
 public class Util {
 
@@ -18,34 +11,5 @@ public class Util {
             return false;
         }
         return true;
-    }
-
-    /**
-     * Get app version from manifest info
-     */
-    public static String getApplicationVersion() {
-        Enumeration resourceEnum;
-        try {
-            resourceEnum = Thread.currentThread().getContextClassLoader().getResources(JarFile.MANIFEST_NAME);
-            while (resourceEnum.hasMoreElements()) {
-                try {
-                    URL url = (URL) resourceEnum.nextElement();
-                    InputStream is = url.openStream();
-                    if (is != null) {
-                        Manifest manifest = new Manifest(is);
-                        Attributes attr = manifest.getMainAttributes();
-                        String version = attr.getValue("Implementation-Version");
-                        if (version != null) {
-                            return version;
-                        }
-                    }
-                } catch (Exception e) {
-                    // Nothing
-                }
-            }
-        } catch (IOException e1) {
-            // Nothing
-        }
-        return null;
     }
 }
